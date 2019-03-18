99bd5d0cccd0edd72fe824b9fcc46ba82602eb18
csdhv9 <csdhv9@mst.edu>
2019-03-17 23:22:08 -0500

    Only unique states in states after state loop.

--- fsa.lisp
+++ fsa.lisp
@@ -48,7 +48,6 @@
 	(terpri)
 	
 	;; make sure every item in letter is in alphabet
-	
 	(setq states (list 'X)) 
 	(setq new_states (list )) 
 	
@@ -57,31 +56,37 @@
 	;; Progress through tape
 	(loop for letter across tape do 
 		(loop for s in states do (format t "S ~A" s)(TERPRI))
-		(loop for s in states do
-
-			
-			(format t "L ~A" letter)
-			  
-			(dolist (func (slot-value machine 'transitions))
-				(setq new_states (append new_states (list (funcall func s letter)))))
-
-			;; remove duplicates
-			
-			(TERPRI))
 		
-		(setq states new_states)
+		(TERPRI)
+		(format t "L ~A" letter)(TERPRI)
+		
+		(loop for s in states do
+			(dolist (func (slot-value machine 'transitions))
+				(setq new_states (append new_states (list (funcall func s letter))))))
+		
+		;; add one of each state to states
+		(setq states (list ))
+			
+		(dolist (item new_states)
+			(if (null (member item states :test #'string-equal)) (setq states (append states (list item)))))
+			
+		;(setq states new_states)
 		(setq new_states (list )))
 		
 	new_states)
 
 
 (defun main () 
-	(setq tape "||||||||||||")
+
+	(setq tape "abc")
 
 	(setq automata 
 		(make-instance 'automaton 
 		:make-states (list (make-instance 'state) (make-instance 'state))
-		:make-transitions (list (lambda (state letter) (format nil "X~AX" state)))))
+		:make-transitions (list 
+			(lambda (state letter) (format nil (if (string-equal letter "a") "B" "C") state))
+			(lambda (state letter) (format nil "~A" state))
+			)))
 
 	(run automata tape))
 
