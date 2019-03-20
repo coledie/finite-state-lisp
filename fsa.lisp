0e7df0684c769273abceb93b405b8eba58ff0a7f
csdhv9 <csdhv9@mst.edu>
2019-03-20 02:55:16 -0500

    Run function now returns states. Add alphabet to demo. Now printing output of run.

--- fsa.lisp
+++ fsa.lisp
@@ -1,12 +1,15 @@
 (defmethod what (obj)
-	;; Make so can take just string or will evaluate lambda
+	;;; TODO -  Make so can take just string or will evaluate lambda
 	(format t (funcall (slot-value obj 'repr) obj))(terpri))
 
+
+;;; TODO - Documentation
 
 (defclass State ()
   ((repr 
 	:initform 
 	(lambda (obj) (format nil "is final: ~A~%" (slot-value obj 'is_final))))
+   ()
    (is_final
 	:accessor state-is_final
 	:initarg :make-final
@@ -21,12 +24,12 @@
    (alphabet
 	:accessor Automaton-alphabet
 	:initarg :make-alphabet
-	:initform (list ))
+	:initform "")
    (states
 	:accessor Automaton-states
 	:initarg :make-states
 	:initform (list ))
-   (transitions                 ; Make into {state: '(lambda lambda ...)}
+   (transitions
     :accessor state-transitions
 	:initarg :make-transitions
 	:initform (list ))))
@@ -48,10 +51,14 @@
 	(terpri)
 	
 	;; make sure every item in letter is in alphabet
+	(loop for letter across tape do 
+		(if (not (position letter (slot-value machine 'alphabet))) 
+			(return-from run "")))
+	
 	(setq states (list 'X)) 
 	(setq new_states (list )) 
 	
-	;; use collect
+	;; TODO  use collect
 	
 	;; Progress through tape
 	(loop for letter across tape do 
@@ -69,25 +76,31 @@
 			
 		(dolist (item new_states)
 			(if (null (member item states :test #'string-equal)) (setq states (append states (list item)))))
-			
-		;(setq states new_states)
+		
+		;;;;; TODO make sure every states is in automaton states
+		;; make automaton-states into a dict {name, state}
+		(dolist (item states)
+			())
+		
 		(setq new_states (list )))
 		
-	new_states)
+	states)
 
 
 (defun main () 
+	;; TODO - Read from file
 
-	(setq tape "abc")
+	(setq tape "ab")
 
 	(setq automata 
 		(make-instance 'automaton 
+		:make-alphabet "abc"
 		:make-states (list (make-instance 'state) (make-instance 'state))
 		:make-transitions (list 
 			(lambda (state letter) (format nil (if (string-equal letter "a") "B" "C") state))
 			(lambda (state letter) (format nil "~A" state))
 			)))
 
-	(run automata tape))
+	(print (run automata tape)))
 
 (main)
